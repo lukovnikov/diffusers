8bbceef5a7417e582581fe846ec2766788f9e7b4
lukovnikov <lukovnikov@users.noreply.github.com>
2022-11-09 18:50:53 +0100

    improved network for CIFAR10

--- examples/unconditional_image_generation/train_unconditional.py
+++ examples/unconditional_image_generation/train_unconditional.py
@@ -251,16 +251,18 @@ def main(args):
             sample_size=args.resolution,
             in_channels=3,
             out_channels=3,
-            layers_per_block=3,
-            block_out_channels=(128, 256, 256),
+            layers_per_block=2,
+            block_out_channels=(128, 256, 256, 256),
             down_block_types=(
                 "DownBlock2D",
-                "AttnDownBlock2D",
                 "DownBlock2D",
+                "AttnDownBlock2D",
+                "AttnDownBlock2D",
             ),
             up_block_types=(
-                "UpBlock2D",
                 "AttnUpBlock2D",
+                "AttnUpBlock2D",
+                "UpBlock2D",
                 "UpBlock2D",
             ),
         )

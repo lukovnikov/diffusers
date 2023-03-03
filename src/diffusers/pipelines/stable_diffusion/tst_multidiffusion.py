c0e8eb520fd2b5d9b22babe9c65c8c3934752b77
Denis Lukovnikov <lukovnikov.denis@gmail.com>
2023-03-03 19:14:36 +0100

    multidiffusion

--- src/diffusers/pipelines/stable_diffusion/tst_multidiffusion.py
+++ src/diffusers/pipelines/stable_diffusion/tst_multidiffusion.py
@@ -1,5 +1,7 @@
 import torch
-from diffusers import StableDiffusionPanoramaPipeline, DDIMScheduler
+from diffusers import StableDiffusionPanoramaPipeline, DDIMScheduler, StableDiffusionPipeline
+from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_multidiffusion_img2img import \
+    StableDiffusionPipelineMultidiffusionImageToImage
 from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_multilevel_multidiffusion import \
     StableDiffusionHighresPipeline
 
@@ -7,9 +9,12 @@ if __name__ == '__main__':
     # for debugging
     model_ckpt = "runwayml/stable-diffusion-v1-5"
     scheduler = DDIMScheduler.from_pretrained(model_ckpt, subfolder="scheduler")
-    pipe = StableDiffusionHighresPipeline.from_pretrained(model_ckpt, scheduler=scheduler, torch_dtype=torch.float16)
-
-    pipe = pipe.to("cuda")
+    pipe1 = StableDiffusionPipeline.from_pretrained(model_ckpt, scheduler=scheduler, torch_dtype=torch.float16).to("cuda")
+    pipe2 = StableDiffusionPipelineMultidiffusionImageToImage.from_pretrained(model_ckpt, scheduler=scheduler, torch_dtype=torch.float16).to("cuda")
 
     prompt = "a photo of the dolomites"
-    image = pipe(prompt).images[0]
+    image = pipe1(prompt).images[0]
+
+    print(image)
+
+    srimage = pipe2(prompt, image, upscale=2, overlap=256).images[0]
